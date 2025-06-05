98c42ac6c19d7b6ab7b77d621c739710f7bfc8c6
Steve Mares <stevee_mares@hotmail.com>
2025-06-04 21:50:33 -0500

    additional notes

--- src/components/poke_cards.jsx
+++ src/components/poke_cards.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react';
 
 export default function PokeCards () {
     const [pokemonList, setPokemonList] = useState([]);
-    const [currentPage, setCurrentPage] = useState(2);
-    const [limit, setLimit] = useState(20);
+    const [currentPage, setCurrentPage] = useState(1); // this sets the starting page
+    const [limit, setLimit] = useState(20); // changing the useState value sets the QTY on pokemon per page
     const offsetCalc = (currentPage - 1) * limit;
     const [offset, setOffset] = useState(0);
 
@@ -38,11 +38,11 @@ export default function PokeCards () {
     return (
         <div>
             {pokemonList.map((pokemon, index) => {
-                let pokeID = (index + offset) + 1
+                let pokeID = (index + offset) + 1 // the +1 eliminates the pokeID from starting from 0 (no pokemon)
                 return (
                 <div className='card w-36 shadow-sm' key={index}>
                     <figure>
-                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`} alt='image'/>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeID}.png`} alt='image'/>
                     </figure>
                     <h2 className='card-title'>{pokemon.name}</h2>
                 </div>
